5a433d55dd9034d6e535214be7c24bc40ad64c44
ChiesaAvventistaTorino <avventisti.torino@gmail.com>
2025-09-05 11:17:09 +0200

    Cambiato orario

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ CHAT_ID = os.getenv("CHAT_ID")
 ITALY_TZ = pytz.timezone("Europe/Rome")
 
 # Orario del post in formato 24h (ora italiana)
-POST_HOUR = 14  # Ora italiana (CET/CEST)
-POST_MINUTE = 00  # Minuto
+POST_HOUR = 11  # Ora italiana (CET/CEST)
+POST_MINUTE = 20  # Minuto
 
 # Inizializza il bot Telegram
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
