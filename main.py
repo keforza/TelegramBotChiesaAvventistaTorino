ce0b6cc00d897bef7928de8087b7a9c23a5d3bb5
ChiesaAvventistaTorino <avventisti.torino@gmail.com>
2025-09-05 11:22:01 +0200

    Pulizia file inutili e consolidamento bot in main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ ITALY_TZ = pytz.timezone("Europe/Rome")
 
 # Orario del post in formato 24h (ora italiana)
 POST_HOUR = 11  # Ora italiana (CET/CEST)
-POST_MINUTE = 20  # Minuto
+POST_MINUTE = 30  # Minuto
 
 # Inizializza il bot Telegram
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
